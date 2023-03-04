42e6129a424f5f868ce39d5ee37f84cef02fc0d6
sougata das <sougata19582001@gmail.com>
2023-03-04 15:49:59 +0530

    Dictionary app Js V2 slick style meanings antoymns and synonyms

--- app.js
+++ app.js
@@ -1,80 +1,155 @@
-const meaningsElement = document.querySelector('.meanings')
-const wordElement = document.querySelector('.word')
-const wordInputElement = document.querySelector('.word-input')
-const searchBtn = document.querySelector('.search-btn')
-const loaderBtn = document.querySelector('.loader-btn')
+const wordElement = document.querySelector(".word");
+const meaningsElement = document.querySelector(".meanings");
+const searchBtn = document.querySelector(".search-btn");
+const searchBar = document.querySelector(".word-input");
+const loaderBtn = document.querySelector(".loader-btn");
+const errorElement = document.querySelector(".error-msg");
+const thesaurusElement = document.querySelector(".thesaurus");
 
+// Loading animation
+function loadingAnimation(isLoading) {
+  if (isLoading) {
+    searchBtn.classList.add("hidden");
+    loaderBtn.classList.remove("hidden");
+  } else {
+    searchBtn.classList.remove("hidden");
+    loaderBtn.classList.add("hidden");
+  }
+}
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '61c3215823msh1ce21b5e6bce66ap117f52jsn985a699a8370',
-		'X-RapidAPI-Host': 'dictionary-by-api-ninjas.p.rapidapi.com'
-	}
+// Fetch Meaning
+const getMeaningResponse = function (word) {
+  loadingAnimation(true);
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then((response) => response.json())
+    .then((response) => {
+      if (typeof response === "object" && response.hasOwnProperty("message")) {
+        displayError(response.message);
+      } else {
+        getMeaning(response);
+        getThesaurusResponse(word);
+      }
+    });
 };
+// Fetch Thesaurus
+const getThesaurusResponse = function (word) {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "61c3215823msh1ce21b5e6bce66ap117f52jsn985a699a8370",
+      "X-RapidAPI-Host": "thesaurus-by-api-ninjas.p.rapidapi.com",
+    },
+  };
 
-function showLoader(){
-	searchBtn.classList.add('hidden')
-	loaderBtn.classList.remove('hidden')
+  fetch(
+    `https://thesaurus-by-api-ninjas.p.rapidapi.com/v1/thesaurus?word=${word}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      getThesaurus(response);
+    });
+};
+//////////////////////////////////////////////////////
+
+// For getting meanings.
+function getMeaning(meaningObject) {
+  const [{ meanings, word }] = meaningObject;
+  console.log(word);
+  console.log(meanings);
+  displayWord(word);
+  loadingAnimation(false);
+  displayMeaning(meanings);
 }
 
-function hideLoader(){
-	searchBtn.classList.remove('hidden')
-	loaderBtn.classList.add('hidden')
+// For displaying errors
+function displayError(errorMessage) {
+  loadingAnimation(false);
+  alert(errorMessage + "🤔");
 }
 
-function getMeaning(word){
-	showLoader()
-	fetch(`https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=${word}`, options)
-		.then(response => response.json())
-		.then(response => {
-			// console.log(Object.keys(response).length)
-			// console.log(response)
-			hideLoader()
-			if (!response.valid){
-				alert("No Results Found!")
-			}
-			const meanings = response.definition.split(/[0-9]. /)
-			for (let i = 0 ; i < meanings.length; i++){
-				if (meanings[i] == ""){
-					meanings.splice(i, 1)
-				}
-			}
-			// console.log(meanings.length)
-			// for (let i = 0 ; i < meanings.length; i++){
-			// 	console.log(`${i+1} : ${meanings[i]}`)
-			// }
+// For getting thesaurus
+function getThesaurus(thesaurusObject) {
+  console.log(thesaurusObject);
+  thesaurusElement.innerHTML = '';
+  const { synonyms, antonyms } = thesaurusObject;
+  const synonymsText =
+    synonyms.length > 0 ? `<span style="color: #188038">Similar: </span>` : ``;
+  const antonymsText =
+    antonyms.length > 0 ? `<span style="color: #d93025">Opposite: </span>` : ``;
+  thesaurusElement.insertAdjacentHTML("beforeend", synonymsText);
+  synonyms.forEach(function (synonym, i) {
+    const synonymsHTML = `
+    <button
+      class="opposite-words btn btn-outline-light"
+      style="
+        border: 1px solid #dadce0;
+        border-radius: 20px;
+        color: black;
+        margin: 5px 0;
+        font-size: 15px;
+    ">
+      ${synonym}
+    </button>
+  `;
+    thesaurusElement.insertAdjacentHTML("beforeend", synonymsHTML);
+  });
 
-			// console.log(meanings)
-			
-			for (let i = 0 ; i < meanings.length; i++){
-				const m = document.createElement("li")
-				m.classList.add("list-group-item")
-				m.classList.add("d-flex")
-				m.classList.add("justify-content-between")
-				m.classList.add("align-items-center")
-				m.innerHTML = meanings[i]
-				meaningsElement.appendChild(m)
-			}
-		})
-		.catch(err => console.error(err));
+  thesaurusElement.insertAdjacentHTML("beforeend", `<br>`);
+  thesaurusElement.insertAdjacentHTML("beforeend", antonymsText);
+  antonyms.forEach(function(antonym, i){
+    const antonymsHTML = `
+    <button
+      class="opposite-words btn btn-outline-light"
+      style="
+        border: 1px solid #dadce0;
+        border-radius: 20px;
+        color: black;
+        margin: 5px 0;
+        font-size: 15px;
+    ">
+      ${antonym}
+    </button>
+    `;
+    thesaurusElement.insertAdjacentHTML('beforeend', antonymsHTML);
+  });
 }
 
-searchBtn.addEventListener('click', function(){
-	removePreviousMeaning()
-	wordElement.textContent = wordInputElement.value
-	wordElement.classList.remove('hidden')
-	getMeaning(wordInputElement.value)
-})
-
-function removePreviousMeaning(){
-	const len = document.querySelectorAll('.list-group-item').length
-	if (len){
-		const elements = document.getElementsByClassName('list-group-item')
-		console.log(elements)
-		while(elements.length > 0){
-			elements[0].parentNode.removeChild(elements[0]);
-		}
-	}
+//Display Word
+function displayWord(word) {
+  wordElement.classList.remove("hidden");
+  wordElement.textContent = word;
+}
+// Display Meanings
+function displayMeaning(meanings) {
+  meaningsElement.innerHTML = "";
+  meanings.forEach(function (meaning, i, object) {
+    const { partOfSpeech, definitions } = meaning;
+    // parts of speech part.
+    const partOfSpeechHtml = `
+    <p class="parts-of-speech" style="font-style: italic; color: grey;">
+      ${partOfSpeech}
+    </p>
+    `;
+    meaningsElement.insertAdjacentHTML("beforeend", partOfSpeechHtml);
+    // definition part.
+    definitions.forEach(function (definition, i, object) {
+      const definitionHtml = `
+      <p class="meaning" style="margin-left: 40px;">
+          ${definition.definition}
+      </p>
+      `;
+      meaningsElement.insertAdjacentHTML("beforeend", definitionHtml);
+    });
+  });
 }
 
+//////////////////////////////////////////////////////
+// Getting input from the user
+function getUserInput() {
+  searchBtn.addEventListener("click", function () {
+    getMeaningResponse(searchBar.value);
+  });
+}
+
+getUserInput();
